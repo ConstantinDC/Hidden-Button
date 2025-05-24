f43c6ca5288d103f99844295f1ed77aa321566d6
Constantin <stilus.office@gmail.com>
2025-05-24 11:02:36 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -2,11 +2,10 @@ const buttons = document.querySelectorAll('.game-button');
 const message = document.getElementById('message');
 
 const winnerIndex = Math.floor(Math.random() * buttons.length);
-const winnerButton = buttons[winnerIndex];
 
 buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
-    if (button === winnerButton) {
+    if (button === buttons[winnerIndex]) {
       message.innerText = 'WIN!';
      } else {
          message.innerText = 'LOSE!';
